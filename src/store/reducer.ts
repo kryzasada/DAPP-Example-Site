a6f27898915d851f9093e47aafa8224643e81c07
krystian <krystian.zasada@op.pl>
2023-10-09 21:29:46 +0200

    Add chainId to state

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -2,24 +2,36 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 type initialState = {
   address: string
+  chainId: string
 }
 
 const initialState: initialState = {
   address: "",
+  chainId: "",
+}
+
+type setPayloadAction = {
+  address: string
+  chainId: string
 }
 
 export const walletSlice = createSlice({
   name: 'wallet',
   initialState: initialState,
   reducers: {
-    set: (state, address: PayloadAction<string>) => {
-      state.address = address.payload
+    set: (state, data: PayloadAction<setPayloadAction>) => {
+      state.address = data.payload.address
+      state.chainId = data.payload.chainId
+    },
+    setChainId: (state, chainId: PayloadAction<string>) => {
+      state.chainId = chainId.payload
     },
     reset: (state) => {
       state.address = ""
+      state.chainId = ""
     }
-  },
+  }
 })
 
-export const { set, reset } = walletSlice.actions
+export const { set, setChainId, reset } = walletSlice.actions
 export default walletSlice.reducer
